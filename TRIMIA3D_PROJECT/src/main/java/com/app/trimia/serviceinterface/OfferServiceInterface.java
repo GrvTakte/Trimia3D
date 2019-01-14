a64893e929ab3aef616ae87adf1397c7f1b97b64
Gaurav <gauravtakte@gmail.com>
2019-01-14 11:57:51 +0530

    OfferServiceInterface Added

--- TRIMIA3D_PROJECT/src/main/java/com/app/trimia/serviceinterface/OfferServiceInterface.java
+++ TRIMIA3D_PROJECT/src/main/java/com/app/trimia/serviceinterface/OfferServiceInterface.java
@@ -1,5 +1,11 @@
 package com.app.trimia.serviceinterface;
 
+import java.util.List;
+
+import com.app.trimia.model.serviceprovider.MaterialSpecializationCategory;
+
 public interface OfferServiceInterface {
+
+	public List<MaterialSpecializationCategory> loadCategory();
 
 }

9ebf72bb464fe918940ef96b0e913f5e8e9990cd
gaurav93 <gaurav93@gaurav>
2019-01-14 20:28:49 +0530

    table created

--- TRIMIA3D_PROJECT/src/main/java/com/app/trimia/main/Trimia3dApplication.java
+++ TRIMIA3D_PROJECT/src/main/java/com/app/trimia/main/Trimia3dApplication.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages="com.app.trimia")
 @EnableJpaRepositories(basePackages= {"com.app.trimia"})
-@EntityScan(basePackages= {"com.app.trimia.model.serviceprovider"})
+@EntityScan(basePackages= {"com.app.trimia.model"})
 public class Trimia3dApplication {
 
 	public static void main(String[] args) {
